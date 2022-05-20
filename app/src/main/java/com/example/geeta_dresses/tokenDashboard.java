c54b73607f4f2e262e75af8d4bc6f1ba57befc00
ShreyasKulkarni899 <sskulkarni899@gmail.com>
2022-05-20 15:47:25 +0530

    Trying to solve the error in product -Shreyas

--- app/src/main/java/com/example/geeta_dresses/tokenDashboard.java
+++ app/src/main/java/com/example/geeta_dresses/tokenDashboard.java
@@ -1,10 +1,15 @@
 package com.example.geeta_dresses;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -30,6 +35,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.geeta_dresses.constants.Constant;
 import com.example.geeta_dresses.models.InquiryModel;
+import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -41,7 +47,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-public class tokenDashboard extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class tokenDashboard extends AppCompatActivity {//implements AdapterView.OnItemSelectedListener {
     private RecyclerView courseRV;
     Constant constant;
     RequestQueue requestQueue;
@@ -73,8 +79,11 @@ public class tokenDashboard extends AppCompatActivity implements AdapterView.OnI
         userName = findViewById(R.id.userNameTokenNumber);
         currentTokenNumber = findViewById(R.id.userCurrentTokenNo);
         Spinner spino = findViewById(R.id.coursesspinner);
-        spino.setOnItemSelectedListener(this);
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spino.setAdapter(adapter);
         //User data part with SP
         userSP = getSharedPreferences("userMetadata", MODE_PRIVATE);
         userName.setText(userSP.getString("userName", ""));
@@ -94,6 +103,7 @@ public class tokenDashboard extends AppCompatActivity implements AdapterView.OnI
         // here we have created new array list and added data to it.
         productsModelArrayList = new ArrayList<>();
 
+
         //Code for the intent from productFinder
         String resultIN, product_nameIN, productIDIN, product_priceIN;
         Bundle extras = getIntent().getExtras();
@@ -109,6 +119,8 @@ public class tokenDashboard extends AppCompatActivity implements AdapterView.OnI
             product_priceIN = extras.getString("productPrice");
             Log.d("Data here Look", String.valueOf(extras));
             productsModelArrayList.add(new productsModel(product_nameIN, "1", product_priceIN, productIDIN));
+            productsModelArrayList.add(new productsModel(product_nameIN, "1", product_priceIN, productIDIN));
+
         }
         // Using Constants
         constant = new Constant();
@@ -155,6 +167,8 @@ public class tokenDashboard extends AppCompatActivity implements AdapterView.OnI
         requestQueue.add(jsonObjectRequest);
 
         // we are initializing our adapter class and passing our arraylist to it.
+
+        Log.d("Product array list here", String.valueOf(productsModelArrayList));
         productAdapter productAdapter = new productAdapter(this, productsModelArrayList);
 
         // below line is for setting a layout manager for our recycler view.
@@ -170,102 +184,238 @@ public class tokenDashboard extends AppCompatActivity implements AdapterView.OnI
             finish();
             onBackPressed();
         });
-        // Create the instance of ArrayAdapter
-        // having the list of courses
-        ArrayAdapter ad
-                = new ArrayAdapter(
-                this,
-                android.R.layout.simple_spinner_item,
-                courses);
-
-        // set simple layout resource file
-        // for each item of spinner
-        ad.setDropDownViewResource(
-                android.R.layout
-                        .simple_spinner_dropdown_item);
+//        // Create the instance of ArrayAdapter
+//        // having the list of courses
+//        ArrayAdapter ad
+//                = new ArrayAdapter(
+//                this,
+//                android.R.layout.simple_spinner_item,
+//                courses);
+//
+//        // set simple layout resource file
+//        // for each item of spinner
+//        ad.setDropDownViewResource(
+//                android.R.layout
+//                        .simple_spinner_dropdown_item);
 
         // Set the ArrayAdapter (ad) data on the
         // Spinner which binds data to spinner
-        spino.setAdapter(ad);
+//        spino.setAdapter(ad);
         //nameBTN code
+//        nameBTN.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Create an alert builder
+//                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+//                builder.setTitle("Name");
+//
+//                // set the custom layout
+//                final View customLayout
+//                        = getLayoutInflater()
+//                        .inflate(
+//                                R.layout.productfinder_card_dialog, null);
+//                builder.setView(customLayout);
+//
+//                // add a button
+//                builder
+//                        .setPositiveButton(
+//                                "OK",
+//                                new DialogInterface.OnClickListener() {
+//
+//                                    @Override
+//                                    public void onClick(
+//                                            DialogInterface dialog,
+//                                            int which) {
+//
+//                                        // send data from the
+//                                        // AlertDialog to the Activity
+//                                        EditText product_id = (EditText) customLayout.findViewById(R.id.prouctIdPF);
+//                                        EditText product_name = (EditText) customLayout.findViewById(R.id.prouctNamePFD);
+//                                        EditText product_price = (EditText) customLayout.findViewById(R.id.prouctPricePFD);
+//                                        Toast.makeText(getApplicationContext(), product_id.getText().toString() + "  " + product_name.getText().toString()+"  "+product_price.getText().toString(),Toast.LENGTH_LONG).show();
+//
+//                                    }
+//                                });
+//
+//                // create and show
+//                // the alert dialog
+//                AlertDialog dialog = builder.create();
+//                dialog.show();
+//            }
+//
+//
+//    });
+//        nameBTN.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Context context = view.getContext();
+//                AlertDialog.Builder builder=new AlertDialog.Builder(view.getContext());
+//                View v= LayoutInflater.from(context).inflate(R.layout.productfinder_card_dialog,null);
+//                TextInputEditText product_id = v.findViewById(R.id.prouctIdPF);
+//                TextInputEditText product_name = v.findViewById(R.id.prouctNamePFD);
+//                TextInputEditText product_price = v.findViewById(R.id.prouctPricePFD);
+//                builder.setView(v);
+//
+//                //code for the positive button
+//                builder.setPositiveButton("Next", (dialog, which) -> {
+//                    Toast.makeText(context,product_id.getText().toString() + "  " + product_name.getText().toString()+"  "+product_price.getText().toString(),Toast.LENGTH_LONG).show();
+////                if ( !product_id.getText().toString().isEmpty() && !product_name.getText().toString().isEmpty() && !product_price.getText().toString().isEmpty()) {
+////                    Toast.makeText(v.getContext(),"Complete the formality", Toast.LENGTH_SHORT).show();
+////                }
+////                else{
+////                    Toast.makeText(v.getContext(),product_id.getText().toString() + "  " + product_name.getText().toString()+"  "+product_price.getText().toString(),Toast.LENGTH_LONG).show();
+////                    productsModelArrayList.add(new productsModel(product_id.getText().toString(), "1", product_price.getText().toString(), product_id.getText().toString()));
+////                }
+//                });
+//                //code for the negative button
+//                builder.setNegativeButton("back", (dialog, which) -> dialog.dismiss());
+//
+//                //output line for the calling
+//                final AlertDialog alertDialog=builder.create();
+//                alertDialog.show();
+//
+//                //designing for buttons
+//                //positive
+//                Button button_positive = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+//                //button_positive.setBackgroundColor(Color.GRAY);
+//                button_positive.setPadding(20, 5, 20, 5);
+//                button_positive.setTextColor(Color.BLACK);
+//                //negative
+//                Button button_negative = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+//                //button_negative.setBackgroundColor(Color.GRAY);
+//                button_negative.setPadding(20, 5, 20, 5);
+//                button_negative.setTextColor(Color.BLACK);
+//
+//
+//
+//            }
+//        });
         nameBTN.setOnClickListener(view -> {
             Intent intent = new Intent(tokenDashboard.this, ProductFinder.class);
             intent.putExtra("activity", "tokenDashboard");
             startActivity(intent);
-            Toast.makeText(getApplicationContext(), "Clicked on Name", Toast.LENGTH_SHORT).show();
         });
-        dateBTN.setOnClickListener(v -> {
-            final Calendar cldr = Calendar.getInstance();
-            int day = cldr.get(Calendar.DAY_OF_MONTH);
-            int month = cldr.get(Calendar.MONTH);
-            int year = cldr.get(Calendar.YEAR);
-            // date picker dialog
-            picker = new DatePickerDialog(tokenDashboard.this,
-                    new DatePickerDialog.OnDateSetListener() {
-                        @Override
-                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                            dateBTN.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-                            String str = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
-                            //setOrder_date_str(str);
+//        nameBTN.setOnClickListener(view -> {
+////            Intent intent = new Intent(tokenDashboard.this, ProductFinder.class);
+////            intent.putExtra("activity", "tokenDashboard");
+////            startActivity(intent);
+////            Toast.makeText(getApplicationContext(), "Clicked on Name", Toast.LENGTH_SHORT).show();
+//            //defination for the dynamic dialog box
+//            AlertDialog.Builder builder=new AlertDialog.Builder(view.getContext());
+//            View v= LayoutInflater.from(view.getContext()).inflate(R.layout.productfinder_card_dialog,null);
+//            EditText product_id = v.findViewById(R.id.prouctIdPF);
+//            EditText product_name = v.findViewById(R.id.prouctNamePFD);
+//            EditText product_price = v.findViewById(R.id.prouctPricePFD);
+//            builder.setView(v);
+//
+//            //code for the positive button
+//            builder.setPositiveButton("Next", (dialog, which) -> {
+//                Toast.makeText(v.getContext(),product_id.getText().toString() + "  " + product_name.getText().toString()+"  "+product_price.getText().toString(),Toast.LENGTH_LONG).show();
+////                if ( !product_id.getText().toString().isEmpty() && !product_name.getText().toString().isEmpty() && !product_price.getText().toString().isEmpty()) {
+////                    Toast.makeText(v.getContext(),"Complete the formality", Toast.LENGTH_SHORT).show();
+////                }
+////                else{
+////                    Toast.makeText(v.getContext(),product_id.getText().toString() + "  " + product_name.getText().toString()+"  "+product_price.getText().toString(),Toast.LENGTH_LONG).show();
+////                    productsModelArrayList.add(new productsModel(product_id.getText().toString(), "1", product_price.getText().toString(), product_id.getText().toString()));
+////                }
+//            });
+//            //code for the negative button
+//            builder.setNegativeButton("back", (dialog, which) -> dialog.dismiss());
+//
+//            //output line for the calling
+//            final AlertDialog alertDialog=builder.create();
+//            alertDialog.show();
+//
+//            //designing for buttons
+//            //positive
+//            Button button_positive = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+//            //button_positive.setBackgroundColor(Color.GRAY);
+//            button_positive.setPadding(20, 5, 20, 5);
+//            button_positive.setTextColor(Color.BLACK);
+//            //negative
+//            Button button_negative = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+//            //button_negative.setBackgroundColor(Color.GRAY);
+//            button_negative.setPadding(20, 5, 20, 5);
+//            button_negative.setTextColor(Color.BLACK);
+//
+//
+//    });
+        dateBTN.setOnClickListener(v ->
 
-                            dataSTR = year + "/" + (monthOfYear + 1) + "/" + dayOfMonth;
+    {
+        final Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        // date picker dialog
+        picker = new DatePickerDialog(tokenDashboard.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        dateBTN.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        String str = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                        //setOrder_date_str(str);
 
-                        }
-                    }, year, month, day);
-            picker.show();
-        });
+                        dataSTR = year + "/" + (monthOfYear + 1) + "/" + dayOfMonth;
 
-        //nextBTN code
-        nextBTN.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), Dashboard.class);
+                    }
+                }, year, month, day);
+        picker.show();
+    });
 
-            inquiryModel = new InquiryModel();
-            inquiryModel.setUserId(userSP.getString("userId", ""));
-            inquiryModel.setUserName(userSP.getString("userName", ""));
-            inquiryModel.setCustomerId("Customer ID");
-            inquiryModel.setTokenNumber(userSP.getString("tokenNumber", ""));
-            inquiryModel.setReason(reasonSTR);
-            if (dataSTR == null) {
-                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-                Date date = new Date();
-                inquiryModel.setDay(dateFormat.format(date));
-            } else {
-                inquiryModel.setDay(dataSTR);
-            }
-            inquiryModel.setEnquired(inquiredSW.isChecked());
-            inquiryModel.setPurchased(purchasedSW.isChecked());
-            inquiryModel.setProductsModelArrayList(productsModelArrayList);
+    //nextBTN code
+        nextBTN.setOnClickListener(view ->
 
-            try {
-                constant = new Constant();
-                object = new JSONObject(inquiryModel.toString());
-                String url1 = constant.getURL() + constant.getPORT() + constant.getGET_TOKEN_DETAILS() + userSP.getString("tokenNumber", "");
+    {
+        Intent intent = new Intent(getApplicationContext(), Dashboard.class);
 
-                jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url1, object, response -> Toast.makeText(tokenDashboard.this, "Inquiry Added", Toast.LENGTH_SHORT).show(), error -> Toast.makeText(tokenDashboard.this, "Failed To Add Inquiry", Toast.LENGTH_SHORT).show());
-                requestQueue.add(jsonObjectRequest);
-            } catch (JSONException e) {
-                Toast.makeText(tokenDashboard.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
-            }
+        inquiryModel = new InquiryModel();
+        inquiryModel.setUserId(userSP.getString("userId", ""));
+        inquiryModel.setUserName(userSP.getString("userName", ""));
+        inquiryModel.setCustomerId("Customer ID");
+        inquiryModel.setTokenNumber(userSP.getString("tokenNumber", ""));
+        inquiryModel.setReason(reasonSTR);
+        if (dataSTR == null) {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            Date date = new Date();
+            inquiryModel.setDay(dateFormat.format(date));
+        } else {
+            inquiryModel.setDay(dataSTR);
+        }
+        inquiryModel.setEnquired(inquiredSW.isChecked());
+        inquiryModel.setPurchased(purchasedSW.isChecked());
+        inquiryModel.setProductsModelArrayList(productsModelArrayList);
 
-            startActivity(intent);
-        });
-    }
+        try {
+            constant = new Constant();
+            object = new JSONObject(inquiryModel.toString());
+            String url1 = constant.getURL() + constant.getPORT() + constant.getGET_TOKEN_DETAILS() + userSP.getString("tokenNumber", "");
 
-    // Performing action when ItemSelected
-    // from spinner, Overriding onItemSelected method
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//        Toast.makeText(getApplicationContext(),
-//                courses[i],
-//                Toast.LENGTH_LONG)
-//                .show();
-        reasonSTR = courses[i];
-    }
+            jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url1, object, response -> Toast.makeText(tokenDashboard.this, "Inquiry Added", Toast.LENGTH_SHORT).show(), error -> Toast.makeText(tokenDashboard.this, "Failed To Add Inquiry", Toast.LENGTH_SHORT).show());
+            requestQueue.add(jsonObjectRequest);
+        } catch (JSONException e) {
+            Toast.makeText(tokenDashboard.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+        }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+        startActivity(intent);
+    });
+}
 
-    }
+// Performing action when ItemSelected
+// from spinner, Overriding onItemSelected method
+//
+//    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+////        Toast.makeText(getApplicationContext(),
+////                courses[i],
+////                Toast.LENGTH_LONG)
+////                .show();
+//        reasonSTR = courses[i];
+//    }
+//
+//
+//    public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//    }
 }
 
 
